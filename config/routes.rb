5285d52dd9add81a7765afa6c9009530b8e32443
Martin Yanchev <myanch200@gmail.com>
2023-09-08 01:10:28 +0100

    Bringing back the old  paths for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '',
+                     path_names: { sign_in: 'login', sign_out: 'logout', registration: 'register', sign_up: 'signup' }
 
   get 'up' => 'rails/health#show', as: :rails_health_check
 
